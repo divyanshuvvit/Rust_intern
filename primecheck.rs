5053fb1a5aaac8912a215ba18c0ed570b16dd17f
Divyanshu Kumar <divyanshukhg2002@gmail.com>
2024-04-01 15:38:31 +0530

    Create primecheck.rs

---
+++ primecheck.rs
@@ -0,0 +1,11 @@
+fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    for i in 2..=n / 2 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}

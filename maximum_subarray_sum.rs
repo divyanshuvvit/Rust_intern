b3209da4e973ca5bad4387269ca8d89696824e02
Divyanshu Kumar <divyanshukhg2002@gmail.com>
2024-04-01 15:40:18 +0530

    Create  maximum_subarray_sum.rs

---
+++ maximum_subarray_sum.rs
@@ -0,0 +1,9 @@
+fn max_subarray_sum(nums: &[i32]) -> i32 {
+    let mut max_sum = nums[0];
+    let mut current_sum = nums[0];
+    for &num in nums.iter().skip(1) {
+        current_sum = num.max(current_sum + num);
+        max_sum = max_sum.max(current_sum);
+    }
+    max_sum
+}

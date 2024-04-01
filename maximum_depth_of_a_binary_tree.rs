0414c0fbdd95089fa86f7af1ce4a6b45093cc0d9
Divyanshu Kumar <divyanshukhg2002@gmail.com>
2024-04-01 15:36:19 +0530

    Create  maximum_depth_of_a_binary_tree.rs

---
+++ maximum_depth_of_a_binary_tree.rs
@@ -0,0 +1,24 @@
+use std::cmp;
+
+struct TreeNode {
+    val: i32,
+    left: Option<Box<TreeNode>>,
+    right: Option<Box<TreeNode>>,
+}
+
+impl TreeNode {
+    fn new(val: i32) -> TreeNode {
+        TreeNode { val, left: None, right: None }
+    }
+}
+
+fn max_depth(root: Option<Box<TreeNode>>) -> i32 {
+    match root {
+        None => 0,
+        Some(node) => {
+            let left_depth = max_depth(node.left);
+            let right_depth = max_depth(node.right);
+            cmp::max(left_depth, right_depth) + 1
+        }
+    }
+}
